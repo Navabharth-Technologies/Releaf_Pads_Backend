d2062625f81a3b878bc4521007b3b2d20f8caf6e
Developer <developer@example.com>
2026-09-04 12:26:08 +0530

    Remove catalog parameters

--- services/whatsappService.js
+++ services/whatsappService.js
@@ -122,10 +122,7 @@ class WhatsAppService {
             text: bodyText
           },
           action: {
-            name: "catalog_message",
-            parameters: {
-              thumbnail_product_retailer_id: "pwf2qmehk"
-            }
+            name: "catalog_message"
           }
         }
       };
